4a4ffe75ab867b821057efb3171a643aaae263d3
radeon <samedov_rodion@niuitmo.ru>
2019-04-15 13:26:59 +0300

    Add index file

---
+++ index.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	api "goTest/api/users"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/api/users", api.GetAllUsers).Methods("GET")
+	r.HandleFunc("/api/users/{id}", api.GetUser).Methods("GET")
+	r.HandleFunc("/api/users", api.AddUser).Methods("POST")
+	r.HandleFunc("/api/users/{id}", api.DeleteUser).Methods("DELETE")
+
+	http.ListenAndServe(":3000", r)
+}
